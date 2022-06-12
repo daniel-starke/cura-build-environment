1ad12ae6976ecf457e1d79bfa42ca88ce5ad69ec
j.spijker@ultimaker.com <jelle spijker>
2022-06-12 22:03:09 +0200

    Use conandata for specific branches and data
    Contributes to CURA-9365

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,15 @@
 import os
 
+from pathlib import Path
 from jinja2 import Template
 
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMakeDeps
 from conan.tools.env import VirtualRunEnv
+from conans import tools
 from conans.errors import ConanException
 
-required_conan_version = ">=1.46.2"
+required_conan_version = ">=1.48"
 
 
 class CuraBuildEnvironemtConan(ConanFile):
@@ -29,15 +31,25 @@ class CuraBuildEnvironemtConan(ConanFile):
         self.folders.build = f"cmake-build-{build_type.lower()}"
         self.folders.generators = os.path.join(self.folders.build, "conan")
 
-    def configure(self):
-        self.options["boost"].header_only = True
-        self.options["*"].shared = True
+    def build_requirements(self):
+        self.tool_requires("ninja/[>=1.10.0]")
+        self.tool_requires("cmake/[>=3.23.0]")
 
-    def requirements(self):
-        self.requires("pynest2d/latest@ultimaker/cura-9365")
-        self.requires("savitar/latest@ultimaker/cura-9365")
-        self.requires("arcus/latest@ultimaker/cura-9365")
-        self.requires("curaengine/latest@ultimaker/cura-9365")
+    def source(self):
+        username = os.environ.get("GIT_USERNAME", None)
+        password = os.environ.get("GIT_PASSWORD", None)
+        for git_src in self.conan_data[self.version]["git"].values():
+            folder = Path(self.source_folder, git_src["directory"])
+            should_clone = folder.exists()
+            git = tools.Git(folder = folder, username = username, password = password)
+            if should_clone:
+                git.checkout(git_src["branch"])
+            else:
+                if username and password:
+                    url = git.get_url_with_credentials(git_src["url"])
+                else:
+                    url = git_src["url"]
+                git.clone(url = url, branch = git_src["branch"], shallow = True)
 
     def generate(self):
         with open(os.path.join(self.source_folder, "cmake", "pyinstaller.cmake.jinja"), "r") as f:
